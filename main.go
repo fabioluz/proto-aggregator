bcc399877da02160595fa3328aa28546687395eb
Fabio Luz <fabio.luz@scarf.sh>
2024-03-15 15:59:19 -0300

    Update environment variables

--- main.go
+++ main.go
@@ -35,6 +35,7 @@ type App struct {
 }
 
 func main() {
+	fmt.Println("Initializing")
 	kafkaConsumer := createKafkaConsumer()
 	sqlConnection := createSqlConnection()
 	storageClient := createStorageClient()
@@ -114,12 +115,39 @@ func createKafkaConsumer() *kafka.Consumer {
 	host := getEnvOrDefault("KAFKA_HOST", "localhost:9094")
 	consumerGroup := getEnvOrDefault("KAFKA_CONSUMER_GROUP", "group-1")
 
-	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+	// create topic
+	adminConfig := &kafka.ConfigMap{
+		"bootstrap.servers": host,
+	}
+	admin, err := kafka.NewAdminClient(adminConfig)
+	if err != nil {
+		log.Fatalf("Failed to create admin client: %v\n", err)
+	}
+
+	defer admin.Close()
+
+	topicConfig := kafka.TopicSpecification{
+		Topic:             "logs",
+		NumPartitions:     1,
+		ReplicationFactor: 1,
+		Config:            map[string]string{},
+	}
+
+	_, err = admin.CreateTopics(context.Background(), []kafka.TopicSpecification{topicConfig})
+	if err != nil {
+		// check if the error indicates that the topic already exists
+		log.Fatalf("Failed to create topic: %s\n", err)
+	}
+
+	// create consumer
+	consumerConfig := &kafka.ConfigMap{
 		"bootstrap.servers":  host,
 		"group.id":           consumerGroup,
 		"auto.offset.reset":  "earliest",
 		"enable.auto.commit": false,
-	})
+	}
+
+	consumer, err := kafka.NewConsumer(consumerConfig)
 	if err != nil {
 		log.Fatalf("Failed to create consumer: %v\n", err)
 	}
@@ -133,10 +161,13 @@ func createKafkaConsumer() *kafka.Consumer {
 }
 
 func createStorageClient() *storage.Client {
-	ctx := context.Background()
-	os.Setenv("STORAGE_EMULATOR_HOST", "http://localhost:4443")
-	client, err := storage.NewClient(ctx, option.WithEndpoint("http://localhost:4443/storage/v1/"))
+	endpoint := getEnvOrDefault("GCS_ENDPOINT", "http://localhost:4443/storage/v1/")
 
+	// make sure storage emulator host is set
+	// it is required by storage mock
+	os.Setenv("STORAGE_EMULATOR_HOST", endpoint)
+
+	client, err := storage.NewClient(context.Background(), option.WithEndpoint(endpoint))
 	if err != nil {
 		log.Fatalf("Error connecting to google storage: %v", err)
 		return nil
